294943dff1cc8754e6500670482e9bed40d60db7
NoobSaiyan <ms19961998@gmail.com>
2020-12-03 15:27:06 +0530

    basic call

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import axios from 'axios'
+import { useQuery } from 'react-query'
+
+interface PokemonResponse {
+  count: number
+  next: string | null
+  previous: string | null
+  results: PokemonResponseResultItem[]
+}
+interface PokemonResponseResultItem {
+  name: string
+  url: string
+}
 
 const HomePage: React.FC = () => {
-  return <h1>HomePage</h1>
+  const queryInfo = useQuery('pokemon', () =>
+    axios
+      .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => res.data.results)
+  )
+
+  return (
+    <>
+      {queryInfo.data?.map((result) => {
+        return <div key={result.name}>{result.name}</div>
+      })}
+    </>
+  )
 }
 export default HomePage
