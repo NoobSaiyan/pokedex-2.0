f2cf392d43d4008c8765ad5938435abb7234ee23
NoobSaiyan <ms19961998@gmail.com>
2020-12-04 17:55:52 +0530

    added chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>,
   document.getElementById('root')
 )
 
