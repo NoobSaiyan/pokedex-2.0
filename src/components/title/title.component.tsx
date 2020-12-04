12be6c152961c123dba3bbf74dfbe2bc5680fb34
NoobSaiyan <ms19961998@gmail.com>
2020-12-04 14:10:58 +0530

    styled title

--- src/components/title/title.component.tsx
+++ src/components/title/title.component.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import './title.style.css'
 
 const Title: React.FC = () => {
-  return <div className='title'>title</div>
+  return (
+    <div className='title'>
+      <div className='titleClip' />
+      <p className='titlespan'>title</p>
+    </div>
+  )
 }
 export default Title
