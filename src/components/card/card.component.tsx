0b0fa36cda5ae9e5951a7879179ddb854dd8d990
NoobSaiyan <ms19961998@gmail.com>
2020-12-14 14:34:26 +0530

    fetching types for small cards

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import * as _ from 'lodash'
 import './card.styles.css'
+import axios from 'axios'
+import { useQuery } from 'react-query'
 
 interface Props {
   genName?: string
   name: string
   id: number
 }
+interface TypesResponse {
+  types: Types[]
+}
+interface Types {
+  slot: string
+  type: {
+    name: string
+    url: string
+  }
+}
+
+const typesFetcher = async (pokiId: string) => {
+  const { data } = await axios.get<TypesResponse>(
+    `https://pokeapi.co/api/v2/pokemon/${pokiId}`
+  )
+  return data.types
+}
 
 const Card: React.FC<Props> = ({ name, id, genName }) => {
+  // formatting name and serial number
   name = _.startCase(_.toLower(name))
   function pad(n: string, width: number, z: string) {
     z = z || '0'
@@ -17,12 +37,14 @@ const Card: React.FC<Props> = ({ name, id, genName }) => {
   }
   const num = pad(id.toString(), 3, '')
 
+  // image tag css properties
   const imgStyles: React.CSSProperties = {
     height: '100px',
     width: '100px',
     marginTop: '10px',
     marginLeft: '20px',
   }
+  const { data: typesData } = useQuery(id, typesFetcher)
 
   return (
     <div className='homeCard'>
@@ -35,6 +57,11 @@ const Card: React.FC<Props> = ({ name, id, genName }) => {
         />
       </div>
       <div className='homeName'>{name}</div>
+      <div className='homeTypes'>
+        {typesData?.map(({ type }) => {
+          return <h3>{type.name}</h3>
+        })}
+      </div>
       <div className='genName'>{genName}</div>
     </div>
   )
